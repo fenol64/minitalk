2b62b9023c3570be2c98a17596009838c5f7f25d
fenol64 <fenol64.dev@gmail.com>
2023-12-08 16:29:30 -0300

    format to intra

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 NAME = minitalk.a
 
 SRCS = server.c client.c
+BONUS_SRCS = server_bonus.c client_bonus.c
 OBJS = $(SRCS:.c=.o)
+BONUS_OBJS = $(BONUS_SRCS:.c=.o)
 
 .c.o:
 	cc -Wall -Wextra -Werror -c $< -o $(<:.c=.o)
@@ -16,6 +18,11 @@ $(NAME): $(OBJS)
 clean:
 	make -C libft clean
 	rm -rf $(OBJS)
+
+bonus: $(BONUS_OBJS)
+	make -C libft
+	cc -Wall -Wextra -Werror -o server server_bonus.o libft/libft.a
+	cc -Wall -Wextra -Werror -o client client_bonus.o libft/libft.a
 
 fclean:	clean
 	make -C libft fclean
---
+++ server_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnascime <fnascime@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 17:24:22 by fnascime          #+#    #+#             */
+/*   Updated: 2023/12/08 16:28:01 by fnascime         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include "libft/includes/libft.h"
+
+void	handler(int signum, siginfo_t *siginfo, void *context)
+{
+	static int	i = 0;
+	static char	c = 0;
+
+	(void)context;
+	if (signum == SIGUSR1)
+		c |= (0 << i);
+	else if (signum == SIGUSR2)
+		c |= (1 << i);
+	i++;
+	if (i == 8)
+	{
+		ft_printf("%c", c);
+		if (c == '\0')
+		{
+			kill(siginfo->si_pid, SIGUSR1);
+			ft_printf("\n");
+		}
+		i = 0;
+		c = 0;
+	}
+}
+
+int	main(void)
+{
+	int					pid;
+	struct sigaction	*sa;
+
+	sa = malloc(sizeof(struct sigaction));
+	sa->sa_sigaction = handler;
+	sa->sa_flags = SA_SIGINFO;
+	pid = getpid();
+	if (pid < 0)
+	{
+		ft_printf("ERRO: Não foi possível obter o PID\n");
+		return (1);
+	}
+	ft_printf("%d\n", pid);
+	while (1)
+	{
+		if (sigaction(SIGUSR1, sa, NULL) < 0)
+			ft_printf("ERRO: Não foi possível obter o PID\n");
+		else if (sigaction(SIGUSR2, sa, NULL) < 0)
+			ft_printf("ERRO: Não foi possível obter o PID\n");
+		pause();
+	}
+	return (0);
+}
