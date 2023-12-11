91875af63d696f99d9ad510e69038c4957120db0
fenol64 <fenol64.dev@gmail.com>
2023-12-11 16:18:02 -0300

    final review

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: fnascime <fnascime@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:24:22 by fnascime          #+#    #+#             */
-/*   Updated: 2023/12/09 12:34:51 by fnascime         ###   ########.fr       */
+/*   Updated: 2023/12/11 16:13:13 by fnascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	handler(int signum, siginfo_t *siginfo, void *context)
 	static int	i = 0;
 	static char	c = 0;
 
-	(void)context;
+	(void) context;
 	if (signum == SIGUSR1)
 		c |= (0 << i);
 	else if (signum == SIGUSR2)
@@ -39,11 +39,11 @@ void	handler(int signum, siginfo_t *siginfo, void *context)
 int	main(void)
 {
 	int					pid;
-	struct sigaction	*sa;
+	struct sigaction	sa;
 
-	sa = malloc(sizeof(struct sigaction));
-	sa->sa_sigaction = handler;
-	sa->sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = &handler;
+	sa.sa_flags = SA_SIGINFO;
 	pid = getpid();
 	if (pid < 0)
 	{
@@ -53,9 +53,9 @@ int	main(void)
 	ft_printf("%d\n", pid);
 	while (1)
 	{
-		if (sigaction(SIGUSR1, sa, NULL) < 0)
+		if (sigaction(SIGUSR1, &sa, NULL) < 0)
 			ft_printf("ERRO: Não foi possível obter o PID\n");
-		else if (sigaction(SIGUSR2, sa, NULL) < 0)
+		else if (sigaction(SIGUSR2, &sa, NULL) < 0)
 			ft_printf("ERRO: Não foi possível obter o PID\n");
 		pause();
 	}
